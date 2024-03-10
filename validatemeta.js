c74684db37e0851af6d4f3f42b33880218d0c050
avicisean d <avicisean.12@gmail.com>
2024-03-10 04:39:08 +0000

    Fix Version

--- validatemeta.js
+++ validatemeta.js
@@ -2,6 +2,7 @@ const chalk = require("chalk")
 
 function validateMeta() {
     console.log(chalk.red("Check If True"));
+    console.log(chalk.blue("Its OVER"));
 }
 
 module.exports = validateMeta
